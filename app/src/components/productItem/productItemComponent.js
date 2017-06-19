42860ada66f1cec5dace143965205eda29bf46e7
Anton Petkov <anton.petkoff@gmail.com>
2017-06-20 00:00:54 +0300

    add product item

---
+++ app/src/components/productItem/productItemComponent.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class ProductItemController {
+  constructor() {
+
+  }
+}
+
+angular.module('eshop').component('esProductItem', {
+  templateUrl: 'templates/productItem/productItem.html',
+  controller: ProductItemController
+});
