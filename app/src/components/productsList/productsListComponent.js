37166c28c038fa12f16ee72c985a525dab4c31eb
Anton Petkov <anton.petkoff@gmail.com>
2017-06-24 00:38:02 +0300

    Products list order style

--- app/src/components/productsList/productsListComponent.js
+++ app/src/components/productsList/productsListComponent.js
@@ -4,6 +4,13 @@ class ProductsListController {
   constructor(ProductsResource, ProductsFilter) {
     this.products = ProductsResource.products;
     this.filterCriteria = ProductsFilter.criteria;
+    this.orders = [{
+      label: 'Цена (нарастващ ред)',
+      key: '+price'
+    }, {
+      label: 'Цена (намаляващ ред)',
+      key: '-price'
+    }];
   }
 }
 
