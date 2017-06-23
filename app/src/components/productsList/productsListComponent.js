7036bab616f6d8e8bdde2d9a7cb788eba42ab3c0
Anton Petkov <anton.petkoff@gmail.com>
2017-06-24 00:52:28 +0300

    Product list order functionality

--- app/src/components/productsList/productsListComponent.js
+++ app/src/components/productsList/productsListComponent.js
@@ -4,13 +4,13 @@ class ProductsListController {
   constructor(ProductsResource, ProductsFilter) {
     this.products = ProductsResource.products;
     this.filterCriteria = ProductsFilter.criteria;
-    this.orders = [{
-      label: 'Цена (нарастващ ред)',
-      key: '+price'
-    }, {
-      label: 'Цена (намаляващ ред)',
-      key: '-price'
-    }];
+    this.orders = {
+      'Цена (нарастващ ред)': '+price',
+      'Цена (намаляващ ред)': '-price',
+      'Оценка (нарастващ ред)': '+stars',
+      'Оценка (намаляващ ред)': '-stars'
+    };
+    this.orderLabels = _.keys(this.orders);
   }
 }
 
