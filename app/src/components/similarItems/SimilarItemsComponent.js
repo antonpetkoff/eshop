69dcd2cf9a408989fb9974fc7aafce87567169ec
Anton Petkov <anton.petkoff@gmail.com>
2017-06-24 11:24:41 +0300

    Component creation automation

---
+++ app/src/components/similarItems/SimilarItemsComponent.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class SimilarItemsController {
+
+}
+
+angular.module('eshop').component('esSimilarItems', {
+  templateUrl: 'templates/similarItems/similarItems.html',
+  controller: SimilarItemsController
+});
+
---
+++ component.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# TAKES A COMPONENT NAME IN CAMEL CASE WITH FIRST CAPITAL CASE
+
+COMPONENTS_DIR=./app/src/components
+COMPONENT=$1
+SMALL="$(echo ${COMPONENT} | cut -c1 | tr A-Z a-z)$(echo ${COMPONENT} | cut -c2-)"
+COMPONENT_DIR=${COMPONENTS_DIR}/${SMALL}
+
+if [ -d "${COMPONENT_DIR}" ]; then
+    echo "Component already exists!"
+    exit 1
+fi
+
+mkdir "${COMPONENT_DIR}"
+
+touch "${COMPONENT_DIR}/${SMALL}.html"
+
+touch "${COMPONENT_DIR}/${SMALL}.css"
+
+echo "'use strict';
+
+class ${COMPONENT}Controller {
+
+}
+
+angular.module('eshop').component('es${COMPONENT}', {
+  templateUrl: 'templates/${SMALL}/${SMALL}.html',
+  controller: ${COMPONENT}Controller
+});" > "${COMPONENT_DIR}/${COMPONENT}Component.js"
+
